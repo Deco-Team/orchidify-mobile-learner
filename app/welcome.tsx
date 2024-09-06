3c997cba688439bb2649a46d39baf2e8b288b1f3
Anh Le Do Duc <ledoducanh03@gmail.com>
2024-09-06 16:57:50 +0700

    [UI][ANHLDD] Update UI login-register screen

--- app/welcome.tsx
+++ app/welcome.tsx
@@ -1,6 +1,5 @@
 import { router } from 'expo-router'
-import { Dimensions } from 'react-native'
-import { Button, View } from 'react-native-ui-lib'
+import { Button, Carousel, View } from 'react-native-ui-lib'
 
 import Banner from '@/assets/images/login-banner.svg'
 import MyText from '@/components/MyText'
@@ -9,50 +8,64 @@ import { useSession } from '@/contexts/ctx'
 
 export default function Welcome() {
   const { login } = useSession()
-  const height = Dimensions.get('screen').height
+
+  const text = [
+    'Cùng khám phá những khóa học chất lượng hàng đầu của chúng tôi',
+    'Dễ dàng lựa chọn khóa học phù hợp với mong muốn của bạn',
+    'Được tham gia học trực tiếp và cấp chứng chỉ đào tạo'
+  ]
   return (
-    <View style={{ flex: 1, alignItems: 'center', paddingHorizontal: height < 668 ? 10 : 100, paddingTop: 100 }}>
-      {/* Responsive paddingTop */}
-      <Banner />
-      <MyText
-        weight={myFontWeight.semiBold}
-        styleProps={{ fontSize: 26, marginTop: height < 668 ? 10 : 100, marginHorizontal: 5 }}
-        text='Chào mừng tới Orchidify'
-      />
-      {/* Responsive marginTop */}
-      <MyText
-        styleProps={{
-          textAlign: 'center',
-          marginBottom: 20,
-          marginTop: 25,
-          color: myTextColor.caption,
-          fontSize: 14
-        }}
-        text='Cùng khám phá những khóa học chất lượng hàng đầu của chúng tôi.'
-      />
-      <Button
-        size='large'
-        style={{
-          marginTop: 45,
-          marginBottom: 20,
-          paddingHorizontal: 125,
-          backgroundColor: myTheme.primary
-        }}
-        labelStyle={{
-          fontFamily: myFontWeight.semiBold,
-          fontSize: 16
-        }}
-        label='Đăng ký'
-      />
-      <MyText
-        weight={myFontWeight.semiBold}
-        styleProps={{ color: myTextColor.primary, fontSize: 16 }}
-        onPress={() => {
-          login()
-          router.replace('/')
-        }}
-        text='Đã có tài khoản'
-      />
+    <View style={{ flex: 1, alignItems: 'center' }}>
+      <Banner style={{ flexGrow: 1 }} />
+      <View style={{ alignItems: 'center', gap: 24, padding: 24 }}>
+        <MyText weight={myFontWeight.semiBold} styleProps={{ fontSize: 26 }} text='Chào mừng tới Orchidify' />
+        <Carousel
+          style={{ flexGrow: 0 }}
+          loop
+          autoplay
+          autoplayInterval={3000}
+          pageControlPosition='under'
+          pageControlProps={{
+            color: myTheme.primary
+          }}
+        >
+          {text.map((text) => (
+            <MyText
+              key={text}
+              styleProps={{
+                textAlign: 'center',
+                color: myTextColor.caption,
+                fontSize: 14
+              }}
+              text={text}
+            />
+          ))}
+        </Carousel>
+        <Button
+          label='Đăng ký'
+          size='large'
+          style={{
+            backgroundColor: myTheme.primary,
+            minWidth: '100%',
+            height: 48,
+            justifyContent: 'center'
+          }}
+          labelStyle={{
+            fontFamily: myFontWeight.bold,
+            fontSize: 16
+          }}
+        />
+
+        <MyText
+          weight={myFontWeight.medium}
+          styleProps={{ color: myTextColor.primary, fontSize: 16 }}
+          onPress={() => {
+            login()
+            router.replace('/')
+          }}
+          text='Đã có tài khoản'
+        />
+      </View>
     </View>
   )
 }
